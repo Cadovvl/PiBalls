41e260bc44cc10dde598becf0d624b0acccb0481
Pavel Sukhov <pasukhov@yandex-team.ru>
2014-10-18 15:16:32 +0400

    Pattern-3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 import pygame
 from pygame.locals import *
@@ -6,12 +6,23 @@ import sys
 import copy
 import time
 
+pygame.init()
+
+size=[1000,640]
+font = pygame.font.Font(pygame.font.get_default_font(), 12)
+screen=pygame.display.set_mode(size)
+pygame.display.set_caption('I\'m PAVEEEEEEL')
+clock=pygame.time.Clock()
 
 
 
-# -------- Основной цикл программы -----------
-
+done = False
+    
 while done==False:
+    for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+                forceQuit = True
     # ОБРАБОТКА ВСЕХ СОБЫТИЙ ДОЛЖНА БЫТЬ ПОД ЭТИМ КОММЕНТАРИЕМ
     
     # ОБРАБОТКА ВСЕХ СОБЫТИЙ ДОЛЖНА НАХОДИТЬСЯ НАД ЭТИМ КОММЕНТАРИЕМ
